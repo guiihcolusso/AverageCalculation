2df05863f9d377d8f5b25034d769e670fc118379
Guilherme Colusso <80711211+guiihcolusso@users.noreply.github.com>
2021-04-01 20:47:16 -0300

    Create AverageCalculation.java

---
+++ AverageCalculation.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class AverageCalculation {
+
+	public static void main(String[] args) {
+		
+		float n1, n2, n3, n4, total, media;
+		String status;
+	
+		
+		java.util.Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Digite a Primeira Nota : ");
+		n1 = scn.nextFloat();
+		
+		System.out.println("Digite a Segunda Nota : ");
+		n2 = scn.nextFloat();
+		
+		System.out.println("Digite a Terceira Nota : ");
+		n3 = scn.nextFloat();
+		
+		System.out.println("Digite a Quarta Nota : ");
+		n4 = scn.nextFloat();
+		
+		
+		scn.close();
+		
+		total = n1 + n2 + n3 + n4;
+		media = total / 4;
+		
+		
+		if (media <= 5) {
+			status = ("Reprovou");			
+		}else {
+			status = ("Passou");
+		}
+		
+		
+		for (int i = 0; i < 100; ++i)  
+		       System.out.println();  
+		
+		System.out.println("--------------------------------------------------");
+		System.out.println("Sua Media É : " + media);
+		System.out.println("O Valor Total de Suas Nota É : " + total);
+		System.out.println("Status : " + status);
+		System.out.println("--------------------------------------------------");
+
+	}
+
+}
